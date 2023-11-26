73d902c8ca32a81bee1339cdb31847ccdbdfc82e
Pedro Augusto Silva Reis <reis.pedro@estudante.ufjf.br>
2023-11-26 15:06:09 -0300

    criar script e definir valores

--- joaoEMaria.js
+++ joaoEMaria.js
@@ -1,27 +1,3 @@
-let maria = 2;
 let joao = 1;
-export function deJoaoParaMaria() {
-    maria += joao;
-    joao = 0;
-  }
-  
-  export function deMariaParaJoao() {
-    joao += maria;
-    maria = 0;
-  }
-  
-  export function getMaria() {
-    return maria;
-  }
-  
-  export function getJoao() {
-    return joao;
-  }
-  
-  export function setMaria(valor) {
-    maria = valor;
-  }
-  
-  export function setJoao(valor) {
-    joao = valor;
-  }
+let maria = 2;
+
