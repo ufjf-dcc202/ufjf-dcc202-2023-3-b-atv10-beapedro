f34f81a33786fe34ee8cd27747362bd9c6a21735
Pedro Augusto Silva Reis <reis.pedro@estudante.ufjf.br>
2023-11-26 14:24:43 -0300

    criar as  de somar e diminuir maças

--- joaoEMaria.js
+++ joaoEMaria.js
@@ -1,2 +1,27 @@
 let maria = 2;
 let joao = 1;
+export function deJoaoParaMaria() {
+    maria += joao;
+    joao = 0;
+  }
+  
+  export function deMariaParaJoao() {
+    joao += maria;
+    maria = 0;
+  }
+  
+  export function getMaria() {
+    return maria;
+  }
+  
+  export function getJoao() {
+    return joao;
+  }
+  
+  export function setMaria(valor) {
+    maria = valor;
+  }
+  
+  export function setJoao(valor) {
+    joao = valor;
+  }
