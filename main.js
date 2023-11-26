3e52f206c906afcaf4432a6eeb735527e99b46fd
Pedro Augusto Silva Reis <reis.pedro@estudante.ufjf.br>
2023-11-26 15:08:50 -0300

    aualização do código main

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import { getMaria, getJoao, deJoaoParaMaria, setJoao } from "./joaoEMaria.js";
+console.log("MAIN");
+setMaria(4);
+setJoao(-2);
+
+let maria = getMaria();
+let joao = getJoao();
+console.log(maria, joao);
+
+deJoaoParaMaria();
+
+maria = getMaria();
+joao = getJoao();
+console.log(maria, joao);
