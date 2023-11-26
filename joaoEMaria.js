81b753f37a0c9b1171d107daf56c2658cdd49ea3
Pedro Augusto Silva Reis <reis.pedro@estudante.ufjf.br>
2023-11-26 15:06:38 -0300

    definir funções

--- joaoEMaria.js
+++ joaoEMaria.js
@@ -1,3 +1,38 @@
 let joao = 1;
 let maria = 2;
 
+function setJoao(valor){
+    if(valor >= 0){
+        joao = valor;
+    }else{
+        joao = 0;
+    }
+}
+
+function setMaria(valor){
+    if(valor >= 0){
+        maria = valor;
+    }else{
+        maria = 0;
+    }
+}
+
+function getMaria(){
+    return maria;
+}
+
+function getJoao(){
+    return joao;
+}
+
+function deMariaParaJoao(){
+    joao = joao + maria;
+    maria = 0;
+}
+
+function deJoaoParaMaria(){
+    maria = maria + joao;
+    joao = 0;
+}
+
+export {getJoao, getMaria, deJoaoParaMaria, deMariaParaJoao, setJoao, setMaria}; 
